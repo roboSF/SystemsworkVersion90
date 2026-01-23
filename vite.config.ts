45d9dc6ad58f97d04d0f95391faecd9af87b8d17
roboSF <mazuch.robo@gmail.com>
2026-01-23 12:02:39 +0100

    Add dirname and fileURLToPath imports to vite.config.ts for path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
+  import { fileURLToPath } from 'url';
+  import { dirname } from 'path';
   import path from 'path';
+
+  const __dirname = dirname(fileURLToPath(import.meta.url));
 
   export default defineConfig({
     plugins: [react()],
